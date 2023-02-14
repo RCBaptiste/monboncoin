e73b3e1b66bd2e6a8376632dae1481c0bc6860a4
RBaptiste <relut.baptiste@gmail.com>
2023-02-14 09:09:34 +0100

    with annoncesModels complete

--- Models/AnnoncesModel.php
+++ Models/AnnoncesModel.php
@@ -1,3 +1,111 @@
 <?php
-
+//On ne se connecte jamais à une table mais on fait des requêtes
 namespace Models;
+
+use PDO;
+use App\Db;
+
+class AnnoncesModel extends Db{
+    //Creation du CRUD 
+
+    // READ
+    //Méthode pour trouver toutes les annonces
+    public static function findAll($order = null, $limit= null){
+        if($order === null){$request = "SELECT * , annonces.title AS title, categories.title as NameCat FROM annonces INNER JOIN categories ON annonces.idCategorie = categories.idCategorie" . $limit;
+        }else{
+            $request = "SELECT * , annonces.title AS title, categories.title as NameCat FROM annonces INNER JOIN categories ON annonces.idCategorie = categories.idCategorie ORDER BY " . $order . " " .$limit; // Attention !! placer toujours des espaces après les commandes SQL !!!
+        }
+        //Version raccourcie de la condition ci-dessus:
+        //$request = "SELECT * ,annonces.title AS title, categories.title AS NameCat FROM annonces INNER JOIN categories ON annonces.idCategorie = categories.idCategorie";
+        //if($order !==null){
+        //    $request .= " ORDER BY " . $order;
+        //}
+        //if($limit !==null){
+        //    $request .= " " . $limit;
+        //}
+
+        //Même chose en ternaire
+        //$order !== null ? $request .= " ORDER BY " . $order : null;
+        //$limit !== null ? $request .= " " .  $limit : null;
+
+        // Même chose mais en ternaire
+        //$order ? $request .= " ORDER BY " . 
+        //$order : null;
+        //$limit ? $request .= " " . $limit : null;
+
+        $response = self::getDb()->prepare($request); //Methode + sécure et + courante
+        $response->execute();
+
+        return $response->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    //Methode pour trouver une annonce par son Id
+    public static function findById($id){
+        $request = "SELECT * FROM annonces WHERE idAnnonce = ?";
+        $response = self::getDb()->prepare($request);
+        $response->execute($id);
+
+        return $response->fetch(PDO::FETCH_ASSOC);
+    }
+
+    //Methode pour trouver toutes les annonces d'un user
+    public static function findByUSer($idUser){
+        $request = "SELECT * FROM annonces WHERE idUser = ?";
+        $response = self::getDb()->prepare($request);
+        $response->execute($idUser);
+        return $response->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    //Methode pour trouvers toutes les annonces par catégorie
+    public static function findByIdCat($idCategorie){
+        $request = "SELECT * FROM annonces WHERE idCategorie = ?";
+        $response = self::getDb()->prepare($request);
+        $response->execute($idCategorie);
+        return $response->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+
+
+    //////////// CREATE /////////////
+
+    //Methode de création d'une annonce
+    public static function create(array $data){
+        //$data est un tableau qui contient les valeurs à insérer en BDD
+        // "INSERT INTO annonces (idUser, idCategorie, title, description, price, image) VALUES (1, 2, tondeuse, max 250m², 150, tondeuse.jpg)"
+        $request = "INSERT INTO annonces (idUser, idCategorie, title, description, price, image) VALUES (?, ?, ?, ?, ?, ?)";
+        $response = self::getDb()->prepare($request);
+        $response->execute($data);
+    }
+
+    // UPDATE \\
+    /** 
+     * Méthode de mise à jour d'une annonce
+     * @param array $data [
+     * idCategorie: int,
+     * title: string,
+     * description: string,
+     * price: int,
+     * image: string,
+     * idAnnonce: int
+     * ]
+    *
+    *
+    */
+    public static function update(array $data){
+        $request = "UPDATE annonces SET idCategorie = ?, title = ?, description = ?, price = ?, image = ? WHERE idAnnonce = ?";
+        $response = self::getDb()->prepare($request);
+        
+
+        return $response->execute($data);
+    }
+
+    // DELETE \\
+    public static function delete(array $id){
+        $request = "DELETE
+        FROM annonces WHERE idAnnonce = ?";
+        $response = self::getDb()->prepare($request);
+        return $response->execute($id);
+    }
+        
+}
+
--- index.php
+++ index.php
@@ -1,6 +1,39 @@
 <?php
 
-require_once('autoloader.php');
-?>
+use Models\AnnoncesModel;
 
-<h1>test</h1>
+require_once('autoloader.php');
+
+//Tester la méthode findAll()
+//$order = "price DESC";
+//$annonces= AnnoncesModel::findAll($order, "LIMIT 2");
+
+//var_dump($annonces);
+
+//Test de la méthode findById()
+//$id = [2];
+//$annonce = AnnoncesModel::findById($id);
+//var_dump($annonce);
+
+//Test de la méthode findByIdUser
+
+//$idUser = [2];
+//$annoncesUser = //AnnoncesModel::findByUSer($idUser);
+//var_dump($annoncesUser);
+//Test de la methode findByIdCat
+
+//$idCategorie =[2];
+//$annoncesCat = AnnoncesModel::findByIdCat($idCategorie);
+
+//var_dump($annoncesCat);
+
+//Test de la méthode create
+
+//$data = [1,2,"tondeuse", "max 250m², moteur electrique", 185, "tondeuse.jpg"];
+//AnnoncesModel::create($data);
+
+// Test de la méthdoe Update()
+$data = [2,"tondeuse à gazon", "max 250m², moteur electrique", 185, "tondeuse.jpg", 4];
+AnnoncesModel::update($data);
+$id = [4];
+AnnoncesModel::delete($id);
